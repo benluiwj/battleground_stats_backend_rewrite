0b511ab7019806e76a527a14428f267cc9cdc147
benluiwj <wenjie.lui@gmail.com>
2022-10-17 15:24:58 +0800

    add django app

---
+++ battleground_stats_backend_rewrite/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for battleground_stats_backend_rewrite project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'battleground_stats_backend_rewrite.settings')
+
+application = get_asgi_application()
